da7e676b0d05ea16e5b253e05644e6e601333302
Carlos Julio Pérez Quizhpe <carlosjulioperez@gmail.com>
2025-11-06 12:54:50 -0300

    AuditAwareImpl

--- microservices/cards/src/main/java/com/carper/cards/CardsApplication.java
+++ microservices/cards/src/main/java/com/carper/cards/CardsApplication.java
@@ -1,9 +1,39 @@
 package com.carper.cards;
 
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+/*@ComponentScans({ @ComponentScan("com.carper.accounts.controller") })
+@EnableJpaRepositories("com.carper.accounts.repository")
+@EntityScan("com.carper.accounts.model")*/
+@EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+@OpenAPIDefinition(
+    info = @Info(
+        title = "Cards microservice REST API Documentation",
+        description = "DemoBank microservice REST API Documentation",
+        version = "v1",
+        contact = @Contact(
+            name = "Carlos Pérez",
+            email = "carlosjulioperez@gmail.com",
+            url = "https://www.linkedin.com/in/carlosjulioperez/"
+        ),
+        license = @License(
+            name = "Apache 2.0",
+            url = "https://www.linkedin.com/in/carlosjulioperez/"
+        )
+    ),
+    externalDocs = @ExternalDocumentation(
+        description = "DemoBank microservice REST API Documentation",
+        url = "https://www.linkedin.com/in/carlosjulioperez/"
+    )
+)
 public class CardsApplication {
 
 	public static void main(String[] args) {
